734c2171f1b0aa23e70ff2427bb8090a3ea071da
Felipe <fpizarro.ux@gmail.com>
2023-09-25 16:07:10 +0200

    primerCommit

---
+++ src/main/java/com/bookapp/backend/model/User.java
@@ -0,0 +1,16 @@
+package com.bookapp.backend.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+
+
+
+
+
+}
